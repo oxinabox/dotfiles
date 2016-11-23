898b03f2e64fd377ca2b540a9a029aa1d18682fa
Ubuntu <ubuntu@baal.novalocal>
2016-11-23 06:39:06 +0000

    =now with some setupscripts

--- setup_SOE.sh
+++ setup_SOE.sh
@@ -1,8 +1,18 @@
 sudo apt-get install tmux htop  mmv
 
+#Neovim
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
 sudo apt-get install python-dev python-pip python3-dev python3-pip
+
 sudo apt-get install silversearcher-ag
+
+# R lang
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
+sudo apt-get update
+sudo apt-get install r-base
+
+
