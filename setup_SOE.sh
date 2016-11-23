e77d6cd6e7686a03042683372f5c1b560d7439f1
Ubuntu <ubuntu@baal.novalocal>
2016-11-23 06:28:04 +0000

    =now with some setupscripts

--- setup_SOE.sh
+++ setup_SOE.sh
@@ -1,2 +1,8 @@
-sudo apt-get install tmux htop siversearcher-ag mmv 
+sudo apt-get install tmux htop  mmv
 
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install silversearcher-ag
