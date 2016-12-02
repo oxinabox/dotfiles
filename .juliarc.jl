d5340a8f4096cab429280d1d156850a48f4a1a46
Lyndon White <oxinabox@ucc.asn.au>
2016-12-02 04:17:15 +0000

    =

--- .juliarc.jl
+++ .juliarc.jl
@@ -1,10 +1,7 @@
-
+#ENV["PYTHON"]=""
 function add_to_path(directory)
 	if isdir(directory)
 		push!(LOAD_PATH, directory)
 	end
 end
-	
-add_to_path("/mnt_volume/phd/prototypes/SenseSplittingWord2Vec/src/")
-add_to_path("/mnt/phd/prototypes/SenseSplittingWord2Vec/src/")
 
