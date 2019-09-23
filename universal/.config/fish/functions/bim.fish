d46aa024cc48425ee0dc836fa76ac8c1894806d3
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-09-23 12:49:23 +0100

    fish aliases

---
+++ universal/.config/fish/functions/bim.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function bim --description 'alias bim=vim'
+	vim  $argv;
+end
---
+++ universal/.config/fish/functions/cim.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function cim --description 'alias cim=vim'
+	vim  $argv;
+end
