2b48122bff68dacd096711496c0af7a403068bb6
Lyndon White <lyndon.white@invenialabs.co.uk>
2021-06-11 21:04:31 +0100

    =Update at end of mac usage

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,7 +1,11 @@
-# Make it only update the Registry 1 in 4 new julia sessions
+# Disable updating registry on `add` (still runs on `up`)
 using Pkg: Pkg
 Pkg.UPDATED_REGISTRY_THIS_SESSION[] = true
 
+# use more colors for displaying Pkg conflicts
+if isdefined(Pkg.Resolve, :CONFLICT_COLORS)
+    append!(Pkg.Resolve.CONFLICT_COLORS,  [21:51; 55:119; 124:142; 160:184; 196:220])
+end
 
 # Setup Revise.jl
 atreplinit() do repl
