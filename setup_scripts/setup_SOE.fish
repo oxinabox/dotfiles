27ece83b28c3029077f99f0f958b3116a88d89d5
Frames White <oxinabox@pop-os.localdomain>
2023-03-15 18:02:59 +0800

    more SOE

--- setup_scripts/setup_SOE.fish
+++ setup_scripts/setup_SOE.fish
@@ -2,6 +2,7 @@ sudo apt-get install tmux htop silversearcher-ag tldr
 sudo apt-get install git cmake build-essential 
 sudo apt-get install neovim
 sudo apt-get install notepadqq
+sudo apt-get install trash-cli
 
 # setup the fuck
 sudo apt install python3-dev python3-pip python3-setuptools
